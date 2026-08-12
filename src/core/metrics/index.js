3da2b25fd58ad3ab9891407618553216c5653546
Punter613 <30910761+Punter613@users.noreply.github.com>
2026-08-12 06:51:51 -0400

    Export probability calibration through metrics spine

--- src/core/metrics/index.js
+++ src/core/metrics/index.js
@@ -2,9 +2,14 @@
  * SKSK ProTech - Metrics Engine Interface Spine
  * Freezes the stable API surface to shield downstream modules from drift.
  */
-const { calculateConfidence, computeDynamicRiskScore } = require('../metrics.engine');
+const {
+  calculateConfidence,
+  computeDynamicRiskScore,
+  calibrateProbabilityArray
+} = require('../metrics.engine');
 
 module.exports = {
   calculateConfidence,
-  computeDynamicRiskScore
+  computeDynamicRiskScore,
+  calibrateProbabilityArray
 };
