1a3b70e56855090b6a8eef2442334fc2343553cd
Punter613 <30910761+Punter613@users.noreply.github.com>
2026-08-16 23:27:44 -0400

    Remove retired route aggregator

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/full-estimate', require('./full-estimate'));
-router.use('/scrape', require('./scrape'));
-router.use('/jobs', require('./jobs'));
-router.use('/parts-lookup', require('./partsLookup'));
-
-module.exports = router;
